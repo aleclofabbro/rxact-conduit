5464deacc580d19a7d6e3d12e81b0c1f4562aa16
aleclofabbro <agiansanti@uniquid.com>
2018-01-11 15:29:50 +0100

    io ajax + init domain

---
+++ src/domain/conduit/api/Article.ts
@@ -0,0 +1,7 @@
+import { GenericError } from './ErrorResponse';
+import Article from '../data/Article';
+export type ArticleRequest = string;
+
+export type ArticleResponse = {
+  article: Article
+} | GenericError;
---
+++ src/domain/conduit/api/ErrorResponse.ts
@@ -0,0 +1,5 @@
+export type GenericError = {
+  errors: {
+    body: string[];
+  }
+};
